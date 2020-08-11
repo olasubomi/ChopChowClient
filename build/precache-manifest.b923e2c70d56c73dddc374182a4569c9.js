6ca74572fe9eb95fb60ada0a35a3a5e7e9172684
olasubomi <olasubomi.awokoya@hotmail.com>
2020-08-11 01:31:51 -0700

    updating build for last commit

---
+++ build/precache-manifest.b923e2c70d56c73dddc374182a4569c9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4be0eece0f6b86ab2118e2fd1c4a70f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ada19dfae02591acd6a3",
+    "url": "/static/css/2.8dd0fb17.chunk.css"
+  },
+  {
+    "revision": "c223c2c84c97e3a0060b",
+    "url": "/static/css/main.ba6563af.chunk.css"
+  },
+  {
+    "revision": "ada19dfae02591acd6a3",
+    "url": "/static/js/2.17666306.chunk.js"
+  },
+  {
+    "revision": "0035112d395b8a7986311ee2106e8940",
+    "url": "/static/js/2.17666306.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c223c2c84c97e3a0060b",
+    "url": "/static/js/main.30b077f1.chunk.js"
+  },
+  {
+    "revision": "f9e2fbcef8aa6859321c",
+    "url": "/static/js/runtime-main.685e71ab.js"
+  },
+  {
+    "revision": "8340d35f551f0a697a5c6f5cf66b7a71",
+    "url": "/static/media/logo2.8340d35f.png"
+  },
+  {
+    "revision": "da8c3317246a31b70fcba4bba71aa32c",
+    "url": "/static/media/no_meal_step_image.da8c3317.png"
+  },
+  {
+    "revision": "70ad3e22a2eb7137ddfbc576e8536247",
+    "url": "/static/media/ola_ola_palm_oil.70ad3e22.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b98b7815f858adf9f148754bf9a9f910.js"
+  "/precache-manifest.b923e2c70d56c73dddc374182a4569c9.js"
 );
 
 self.addEventListener('message', (event) => {
