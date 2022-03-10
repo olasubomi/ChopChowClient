77f13ce20c45b0e949b70bd210f1ce72fd2c13bd
olasubomi <olasubomi.awokoya@hotmail,com>
2022-03-09 16:24:51 -0800

    Pushing build of last commit

---
+++ build/precache-manifest.56bbe10e409c2cab73062a1d32dd5124.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5646dc2fd573558630e1b9ae77cbd001",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6685788487f257af2ded",
+    "url": "/static/css/2.8dd0fb17.chunk.css"
+  },
+  {
+    "revision": "4ccf0ba4338e36b1fec2",
+    "url": "/static/css/main.151d660e.chunk.css"
+  },
+  {
+    "revision": "6685788487f257af2ded",
+    "url": "/static/js/2.72dbf868.chunk.js"
+  },
+  {
+    "revision": "530d8a58b0bdb813428283aa6d2069d8",
+    "url": "/static/js/2.72dbf868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ccf0ba4338e36b1fec2",
+    "url": "/static/js/main.52199708.chunk.js"
+  },
+  {
+    "revision": "f9e2fbcef8aa6859321c",
+    "url": "/static/js/runtime-main.685e71ab.js"
+  },
+  {
+    "revision": "23a343fa217079012d8b67553e28f860",
+    "url": "/static/media/CC_Logo_no_bg.23a343fa.png"
+  },
+  {
+    "revision": "920586b26175627b52ed2ff4d775bb80",
+    "url": "/static/media/food_prep.920586b2.jpg"
+  },
+  {
+    "revision": "6702032bf979f0554c9ff8d93fef7f9e",
+    "url": "/static/media/grocery_bag.6702032b.jpg"
+  },
+  {
+    "revision": "8340d35f551f0a697a5c6f5cf66b7a71",
+    "url": "/static/media/logo2.8340d35f.png"
+  },
+  {
+    "revision": "da8c3317246a31b70fcba4bba71aa32c",
+    "url": "/static/media/no_meal_step_image.da8c3317.png"
+  },
+  {
+    "revision": "70ad3e22a2eb7137ddfbc576e8536247",
+    "url": "/static/media/ola_ola_palm_oil.70ad3e22.jpg"
+  }
+]);
