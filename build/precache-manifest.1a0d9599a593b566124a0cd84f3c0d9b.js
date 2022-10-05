3686dbff04413677e8b249ecfe6aa8b7168dcfcb
olasubomi <olasubomi.awokoya@hotmail,com>
2022-10-05 14:36:01 -0700

    Pushing a package.json update max react version of 17.0.50, and updating build to test on heroku

---
+++ build/precache-manifest.1a0d9599a593b566124a0cd84f3c0d9b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "464c7ded07fb94a580a7",
+    "url": "/static/css/main.37b1425f.chunk.css"
+  },
+  {
+    "revision": "464c7ded07fb94a580a7",
+    "url": "/static/js/main.d0eacff3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b341618439d8b9847ab4",
+    "url": "/static/css/2.8c1ba396.chunk.css"
+  },
+  {
+    "revision": "b341618439d8b9847ab4",
+    "url": "/static/js/2.66bb4335.chunk.js"
+  },
+  {
+    "revision": "da8c3317246a31b70fcba4bba71aa32c",
+    "url": "/static/media/no_meal_step_image.da8c3317.png"
+  },
+  {
+    "revision": "23a343fa217079012d8b67553e28f860",
+    "url": "/static/media/CC_Logo_no_bg.23a343fa.png"
+  },
+  {
+    "revision": "8340d35f551f0a697a5c6f5cf66b7a71",
+    "url": "/static/media/logo2.8340d35f.png"
+  },
+  {
+    "revision": "70ad3e22a2eb7137ddfbc576e8536247",
+    "url": "/static/media/ola_ola_palm_oil.70ad3e22.jpg"
+  },
+  {
+    "revision": "d4b6bfaccc4ad07ce6086b8e34f1a04b",
+    "url": "/static/media/banner-3.d4b6bfac.png"
+  },
+  {
+    "revision": "6702032bf979f0554c9ff8d93fef7f9e",
+    "url": "/static/media/grocery_bag.6702032b.jpg"
+  },
+  {
+    "revision": "6dce1400b383b5a27942c75ab6cef7a2",
+    "url": "/static/media/food.6dce1400.png"
+  },
+  {
+    "revision": "14fadc1fdbeb93f479cd703bfe91c734",
+    "url": "/static/media/utensil.14fadc1f.png"
+  },
+  {
+    "revision": "a52aa050d63fd3e95cc25cc3a16954c3",
+    "url": "/static/media/shop.a52aa050.png"
+  },
+  {
+    "revision": "ee861c448f8bf4807c15cfb224b36f97",
+    "url": "/static/media/banner-1.ee861c44.png"
+  },
+  {
+    "revision": "dc12c874b368c04227ca9bfa1a35241d",
+    "url": "/static/media/banner-2.dc12c874.png"
+  },
+  {
+    "revision": "360f0da461d294f9405ddd7b7c4a4e48",
+    "url": "/index.html"
+  }
+];
